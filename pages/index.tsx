664246a5f3759d14f8a85f1e967202262aba924a
Juan Briceno <juanbri@gmx.com>
2023-01-21 08:54:36 +0000

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,26 +15,26 @@ export default function Home() {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageUrls, setImageUrls] = useState([]);
 
-  const imagesListRef = ref(storage, "images/");
-  const uploadFile = () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrls((prev) => [...prev, url]);
-      });
-    });
-  };
+  // const imagesListRef = ref(storage, "images/");
+  // const uploadFile = () => {
+  //   if (imageUpload == null) return;
+  //   const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+  //   uploadBytes(imageRef, imageUpload).then((snapshot) => {
+  //     getDownloadURL(snapshot.ref).then((url) => {
+  //       setImageUrls((prev) => [...prev, url]);
+  //     });
+  //   });
+  // };
 
-  useEffect(() => {
-    listAll(imagesListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageUrls((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, []);
+  // useEffect(() => {
+  //   listAll(imagesListRef).then((response) => {
+  //     response.items.forEach((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImageUrls((prev) => [...prev, url]);
+  //       });
+  //     });
+  //   });
+  // }, []);
 
   return (
     <>
@@ -44,7 +44,7 @@ export default function Home() {
         url={undefined}
         image={"logo/svg"}
       />
-      <input
+      {/* <input
         type="file"
         onChange={(event) => {
           setImageUpload(event.target.files[0]);
@@ -53,7 +53,7 @@ export default function Home() {
       <button onClick={uploadFile}> Upload Image</button>
       {imageUrls.map((url, i) => {
         return <img key={i} src={url} />;
-      })}
+      })} */}
     </>
   );
 }
